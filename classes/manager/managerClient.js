fee3877d3ade88350887f5b19638edbef9f58cf6
pauliclark <paul@venuescanner.com>
2021-05-03 15:54:41 +0100

    Prevent unauthorised error

--- classes/manager/managerClient.js
+++ classes/manager/managerClient.js
@@ -77,7 +77,7 @@ export class ManagerClient {
   connect ({ port }) {
     this.log.log(`Connect socket to ${this.remoteIP}:${port}`)
     this.autoReconnect = true
-    this.socket = client(`${this.remoteIP}:${port}`, { transports: ['websocket'] })
+    this.socket = client(`${this.remoteIP}:${port}`, { rejectUnauthorized: false, transports: ['websocket'] })
 
     this.socket.on('connect', () => { this.declareMyself() })
 
